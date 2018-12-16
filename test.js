018ed80163fb518f88e906719d2bb1996b1f0ac6
ChristopherStavros <christopher.kurkoski@gmail.com>
2018-12-16 17:28:43 -0600

    test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log('Hello World')
+var x = 1
+var y = 4
+console.log(x + y)
+//global.console.log('Hello')
+
+console.log(process.platform)
+console.log(process.arch)
+console.log(process.env.NUMBER_OF_PROCESSORS)
+//console.log(global)
+
