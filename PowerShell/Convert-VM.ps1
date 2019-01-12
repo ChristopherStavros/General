b0acc97732d5fb6a1786c0c075d7af4927ea30aa
N3rlens <chriskurkoski@hotmailcom>
2019-01-12 09:54:57 -0600

    DOcker_notes2

---
+++ PowerShell/Convert-VM.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module 'C:\Program Files\Microsoft Virtual Machine Converter\MvmcCmdlet.psd1'
+
+$source = "C:\Users\chris\Downloads\kali-linux-2018-4-vm-amd64-7z\Kali-Linux-2018.4-vm-amd64\Kali-Linux-2018.4-vm-amd64.vmdk"
+$dest = "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
+ConvertTo-MvmcVirtualHardDisk -SourceLiteralPath $source -VhdType DynamicHardDisk -VhdFormat vhdx -destination $dest
